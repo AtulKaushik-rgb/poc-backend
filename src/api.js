4936069c0f38a576fa55cd248ded012cf53a9ccb
Atul Kaushik <kaushikatul033@gmail.com>
2021-04-29 09:14:53 +0530

    Fixed header errors

--- src/api.js
+++ src/api.js
@@ -28,17 +28,27 @@ router.get("/items/search", async function (req, res, next) {
     let data = null;
     try {
       const output = dbData;
+      console.log('req.query is',req.query);
       if (req.query.q !== "all")
       {
-        data = output.default.items.find(it => it.title.toLowerCase() === req.query.q.toLowerCase())
+        //data = output.default.items.find(it => it.title.toLowerCase() === req.query.q.toLowerCase())
+        data = output.default.items.filter(
+          (it) => it.title.toLowerCase() === req.query.q.toLowerCase()
+        );
       }else data = output;
   
       if (req.query.max) {
-        data = data.filter(
+
+        data = data.default.items.filter(
           (items) => parseInt(items.prices) <= parseInt(req.query.max)
         );
+
+        console.log('filtered with max',data);
   
         if (req.query.brands && req.query.brands.length != 5) {
+          console.log('brands is',req.query.brands);
+          console.log('brands length is',req.query.brands.length);
+          console.log('brands data to be filtered is',data);
           var numberArray = JSON.parse(req.query.brands).map(function (item) {
             return parseInt(item);
           });
@@ -50,6 +60,7 @@ router.get("/items/search", async function (req, res, next) {
         }
   
         if (req.query.star != 1)
+          console.log('star is',req.query.max);
           data = data.filter(
             (items) => parseInt(items.customer_rating) >= parseInt(req.query.star)
           );
